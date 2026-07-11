a1cfdcf83a6c3d4a171017db5af06c4c4b305493
4awmy <o.metwall06131@student.aast.edu>
2026-07-11 19:41:38 +0300

    config: point API URL to production Render backend

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "https://convene-backend-6hzd.onrender.com";
 
 // Make constants and helpers global so all dynamically loaded views can access them
 window.API = API;
